ee5bbc973cd6c66a52dd89bb5c5a236cc40ab7c3
Oscar Moreno <oscarpol95@gmail.com>
2025-06-17 16:35:48 -0500

    feat(controller): implement createProject method for project creation

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,6 +1,24 @@
 import type { Request, Response } from "express";
+import Project from "../models/Projects";
 
 export class ProjectController {
+	static createProject = async (req: Request, res: Response) => {
+		const project = new Project(req.body);
+
+		try {
+			await project.save();
+			res.status(201).json({
+				message: "Proyecto creado correctamente",
+				project,
+			});
+		} catch (error) {
+			res.status(500).json({
+				message: "Error al crear el proyecto",
+				error,
+			});
+		}
+	};
+
 	static getAllProjects = async (req: Request, res: Response) => {
 		res.send("todos los proyectos");
 	};
