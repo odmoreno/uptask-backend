061640bfb62dd9909326948b7cd048b455471137
Oscar Moreno <oscarpol95@gmail.com>
2025-06-18 11:25:30 -0500

    feat(routes): enhance project routes with input validation for project creation and updates

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,61 @@
 import { Router } from "express";
+import { body, param } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
+import { TaskController } from "../controllers/TaskController";
 
 const router = Router();
 
-router.post("/", ProjectController.createProject);
+router.post(
+	"/",
+	body("projectName")
+		.notEmpty()
+		.withMessage("El nombre del proyecto es obligatorio"),
+	body("clientName")
+		.notEmpty()
+		.withMessage("El nombre del Cliente es obligatorio"),
+	body("description")
+		.notEmpty()
+		.withMessage("La descripcion del proyecto es obligatorio"),
+	handleInputErrors,
+	ProjectController.createProject
+);
+
 router.get("/", ProjectController.getAllProjects);
+
+router.get(
+	"/:id",
+	param("id").isMongoId().withMessage("ID no válida"),
+	handleInputErrors,
+	ProjectController.getProjectById
+);
+
+router.put(
+	"/:id",
+	param("id").isMongoId().withMessage("ID no válida"),
+	body("projectName")
+		.notEmpty()
+		.withMessage("El nombre del proyecto es obligatorio"),
+	body("clientName")
+		.notEmpty()
+		.withMessage("El nombre del Cliente es obligatorio"),
+	body("description")
+		.notEmpty()
+		.withMessage("La descripcion del proyecto es obligatorio"),
+	handleInputErrors,
+	ProjectController.updateProject
+);
+
+router.delete(
+	"/:id",
+	param("id").isMongoId().withMessage("ID no válida"),
+	handleInputErrors,
+	ProjectController.deleteProject
+);
+
+/** Routes for tasks */
+router.post("/:projectId/tasks", TaskController.createTask);
+
+
 
 export default router;
