b31e72d0067eb204aaade1610ee25ed7cd7864a3
Oscar Moreno <oscarpol95@gmail.com>
2025-06-18 11:25:26 -0500

    feat(models): update Project model to use PopulatedDoc for tasks and improve type definitions

--- src/models/Projects.ts
+++ src/models/Projects.ts
@@ -1,30 +1,42 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, PopulatedDoc } from "mongoose";
+import { ITask } from "./Task";
 
-export type ProjectType = Document & {
+//export type ProjectType = Document & {
+export interface IProject extends Document {
 	projectName: string;
 	clientName: string;
 	description: string;
-};
+	tasks: PopulatedDoc<ITask & Document>[];
+}
 
 //https://mongoosejs.com/docs/schematypes.html enlaces de los tipos de datos
-const ProjectSchema: Schema = new Schema({
-	projectName: {
-		type: String,
-		required: true,
-		trim: true,
+const ProjectSchema: Schema = new Schema(
+	{
+		projectName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		clientName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			require: true,
+			trim: true,
+		},
+		tasks: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Task",
+			},
+		],
 	},
-	clientName: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	description: {
-		type: String,
-		require: true,
-		trim: true,
-	},
-});
+	{ timestamps: true }
+);
 
-const Project = mongoose.model<ProjectType>("Project", ProjectSchema);
+const Project = mongoose.model<IProject>("Project", ProjectSchema);
 
 export default Project;
