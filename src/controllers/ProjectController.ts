269c33cb3ab7fc9e5763cccaf2373a67d9983b84
Oscar Moreno <oscarpol95@gmail.com>
2025-06-18 11:25:12 -0500

    feat(controller): enhance ProjectController with CRUD operations for projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -20,6 +20,59 @@ export class ProjectController {
 	};
 
 	static getAllProjects = async (req: Request, res: Response) => {
-		res.send("todos los proyectos");
+		try {
+			const projects = await Project.find();
+			res.json(projects);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	static getProjectById = async (req: Request, res: Response) => {
+		const { id } = req.params;
+		try {
+			const project = await Project.findById(id);
+			res.json(project);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	static updateProject = async (req: Request, res: Response): Promise<void> => {
+		const { id } = req.params;
+		const { projectName, clientName, description } = req.body;
+		try {
+			const project = await Project.findByIdAndUpdate(
+				id,
+				{ projectName, clientName, description },
+				{ new: true }
+			);
+
+			if (!project) {
+				const error = new Error("Proyecto no encontrado");
+				res.status(404).json({ error: error.message });
+				return;
+			}
+			res.json(project);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	static deleteProject = async (req: Request, res: Response): Promise<void> => {
+		const { id } = req.params;
+		try {
+			const project = await Project.findById(id);
+			if (!project) {
+				const error = new Error("Proyecto no encontrado");
+				res.status(404).json({ error: error.message });
+				return;
+			}
+			//await Project.findByIdAndDelete(id);
+			await project.deleteOne(),
+			res.json({ message: "Proyecto eliminado correctamente" });
+		} catch (error) {
+			console.log(error);
+		}
 	};
 }
