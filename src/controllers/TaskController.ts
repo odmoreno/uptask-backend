2575ab660df8a8a817ba12ebe3dfa47c8fd36786
Oscar Moreno <oscarpol95@gmail.com>
2025-06-18 11:59:46 -0500

    feat(controller): refactor createTask method to use req.project.id and improve task creation logic

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -3,10 +3,14 @@ import Task from "../models/Task";
 
 export class TaskController {
 	static createTask = async (req: Request, res: Response) => {
-		const { projectId } = req.params;
 		const { name, description } = req.body;
 		try {
-			const task = await Task.create({ name, description, project: projectId });
+			const task = new Task({ name, description, project: req.project.id });
+			req.project.tasks.push(task.id);
+			// AllSettled es una promesa que se resuelve cuando todas las promesas se resuelven
+			// Optimiza el tiempo de respuesta, paraleliza las operaciones
+			await Promise.allSettled([task.save(), req.project.save()]);
+			res.send(task);
 		} catch (error) {
 			console.log(error);
 		}
